d97c6217ce911ea42adc5780b2d969dd7f2424f4
dedeheri <dedeherii60@gmail.com>
2024-10-06 04:08:52 +0700

    fix

--- src/app/api/accounts/signin/route.tsx
+++ src/app/api/accounts/signin/route.tsx
@@ -1,25 +1,27 @@
 import { NextResponse } from "next/server";
-import { IssueData, RefinementCtx, z } from "zod";
+import { z, IssueData, RefinementCtx } from "zod";
 import { prisma } from "@/lib/primsa-db";
-
 import { compare } from "bcrypt";
 
-interface IContex extends RefinementCtx {
+// Define a custom context interface for validation issues
+interface IContext extends RefinementCtx {
   addIssue: (issue: IssueData) => void;
 }
 
-export const bodyRawSchema = z
+// Define the schema with validation rules
+const bodySchema = z
   .object({
     email: z.string().min(5, "Email diperlukan").email("Email Tidak Valid"),
     password: z
       .string()
       .min(8, "Kata sandi harus mengandung minimal 8 karakter"),
   })
-  .superRefine(async ({ email, password }, ctx: IContex) => {
+  .superRefine(async ({ email, password }, ctx: IContext) => {
     const account = await prisma.account.findUnique({
       where: { email },
     });
 
+    // Check if the email exists
     if (!account) {
       ctx.addIssue({
         message: "Email belum terdafar",
@@ -40,18 +42,23 @@ export const bodyRawSchema = z
     }
   });
 
+// Define the POST handler
 export const POST = async (request: Request) => {
   try {
-    const bodyRaw = await bodyRawSchema.parseAsync(await request.json());
+    // Parse and validate the incoming request body
+    const body = await bodySchema.parseAsync(await request.json());
 
+    // Retrieve the account by email
     const account = await prisma.account.findUnique({
-      where: { email: bodyRaw.email },
+      where: { email: body.email },
     });
 
+    // Retrieve the user associated with the account
     const user = await prisma.user.findFirst({
       where: { accountId: account?.id },
     });
 
+    // Respond with the user data
     return NextResponse.json(
       {
         message: "Berhasil",
@@ -60,8 +67,9 @@ export const POST = async (request: Request) => {
       { status: 200 }
     );
   } catch (error: any) {
+    // Handle errors and respond with an appropriate message
     return NextResponse.json(
-      { message: "Internal server error", error: error },
+      { message: "Internal server error", error: error.errors || error },
       { status: 500 }
     );
   }
